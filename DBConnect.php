04089a5802a96e05b1b8167a1158301e27de68f4
Alexandre DELAPIERRE <alexandredelapierre@gmail.com>
2026-02-16 20:19:15 +0100

    refactor: add .gitignore with _config.php

--- DBConnect.php
+++ DBConnect.php
@@ -1,18 +1,16 @@
 <?php
 
+require_once '_config.php';
+
 class DBConnect
 {
     private $pdo;
-    private $host = 'localhost';
-    private $dbname = 'creez-un-outil-de-gestion-de-contacts-en-ligne-de-commande';
-    private $user = 'root';
-    private $password = '';
 
     public function __construct()
     {
         try {
             // Construction du DSN (Data Source Name)
-            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8";
+            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8";
             
             // Options pour activer les erreurs PDO et le mode de récupération par défaut
             $options = [
@@ -20,7 +18,7 @@ class DBConnect
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             ];
 
-            $this->pdo = new PDO($dsn, $this->user, $this->password, $options);
+            $this->pdo = new PDO($dsn, DB_USER, DB_PASSWORD, $options);
             
         } catch (PDOException $e) {
             throw new Exception("Erreur de connexion à la base de données.");
